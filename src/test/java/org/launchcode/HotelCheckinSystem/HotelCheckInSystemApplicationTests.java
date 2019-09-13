32738f7773de302ffc56818055f237ce4cb71a01
Priyaj2776 <Priyaj2776@gmail.com>
2019-09-12 22:18:48 -0500

    Initial Commit

---
+++ src/test/java/org/launchcode/HotelCheckinSystem/HotelCheckInSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.HotelCheckinSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelCheckInSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
